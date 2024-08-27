9b76829c164392d1aa6092352729583bdc6eaa16
Leonardo Joau <joauleonardo@icloud.com>
2024-08-27 00:20:13 -0300

    Optional LSP and remove lightline

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+# Prompt user to enable LSP
+read -p "Do you want to enable LSP? (y/n): " enable_lsp
+
+# Set up Neovim configuration
 rm -rf ~/.config/nvim
 ln -s $(pwd) ~/.config/nvim
+
+# Check user's input and delete lsp.lua if LSP is not enabled
+if [[ "$enable_lsp" =~ ^[Yy]$ ]]; then
+  echo "LSP is enabled."
+else
+  echo "LSP is not enabled. Removing lua/mynvim/lazy/lsp.lua."
+  rm -f $(pwd)/lua/mynvim/lazy/lsp.lua
+fi
