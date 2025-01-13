1e99ee9e77ffe1da3e27ed8f3f79b1ea4df4b500
Leonardo Joau <joauleonardo@icloud.com>
2025-01-13 10:21:23 -0300

    vim test

--- lua/mynvim/lazy/test.lua
+++ lua/mynvim/lazy/test.lua
@@ -1,13 +1,15 @@
 return {
-  'vim-test/vim-test',
-  config = function()
-    vim.cmd [[ let test#strategy = "vimux" ]]
+  {
+    'vim-test/vim-test',
+    dependencies = { 'benmills/vimux' },
+    config = function()
+      vim.cmd [[ let test#strategy = "vimux" ]]
 
-    vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>T', ':TestFile<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>a', ':TestSuite<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast<CR>', { noremap = true, silent = true })
-  end,
-
-  'benmills/vimux'
+      vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>T', ':TestFile<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>a', ':TestSuite<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast<CR>', { noremap = true, silent = true })
+    end,
+  },
+  'benmills/vimux',
 }
