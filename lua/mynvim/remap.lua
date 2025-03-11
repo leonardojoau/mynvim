f6bdbc136000669ef204d663c403c91423829e47
Leonardo Joau <joauleonardo@icloud.com>
2025-03-11 17:34:26 -0300

    Remap file tree explorer shortcut

--- lua/mynvim/remap.lua
+++ lua/mynvim/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("t", "<C-o>", [[<C-\><C-n>:b#<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
