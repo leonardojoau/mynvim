87206c02741a3fdb097f342637d2c93f1daa6dbb
Leonardo Joau <joauleonardo@icloud.com>
2025-04-05 17:47:25 -0300

    Improve treesitter configuration

--- lua/mynvim/lazy/treesitter.lua
+++ lua/mynvim/lazy/treesitter.lua
@@ -1,20 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	run = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+        "nvim-treesitter/playground",
+	},
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "c", "cpp", "lua", "rust", "python" },
 			highlight = {
 				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
 			},
 			textobjects = {
 				select = {
